cc800ac900e09dd143ec37bb9e5b3d7669d73aa0
feritiro <fernishimura@hotmail.com>
2025-07-17 00:11:38 +0900

    app should start when screen turns on, and updated database

---
+++ app/src/main/java/com/itiro/jp_notify/BootReceiver.kt
@@ -0,0 +1,19 @@
+package com.itiro.jp_notify
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            Log.d("BootReceiver", "Boot completed, starting jp_notify")
+
+            // Inicia a MainActivity
+            val i = Intent(context, MainActivity::class.java)
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(i)
+        }
+    }
+}
